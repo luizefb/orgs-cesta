94d1f78648d05f0e7e3b870ec14031adba7cfd49
Luiz <hotemaki@gmail.com>
2024-06-21 21:28:53 -0300

    update lista de itens

--- src/mocks/cesta.js
+++ src/mocks/cesta.js
@@ -1,4 +1,9 @@
 import logo from '../../assets/logo.png';
+import tomate from '../../assets/frutas/Tomate.png';
+import brocolis from '../../assets/frutas/Brócolis.png';
+import batata from '../../assets/frutas/Batata.png';
+import abobora from '../../assets/frutas/Abóbora.png';
+import pepino from '../../assets/frutas/Pepino.png';
 
 const cesta = {
     topo: {
@@ -10,7 +15,33 @@ const cesta = {
         nomeFazenda: "Fazendas Schrute",
         descricao: "Uma cesta com produtos selecionados com excelente qualidade, colhidos por Mose Schrute.",
         preco: "R$ 26,98",
-    }
+        botao: "Comprar",
+    },
+    itens: {
+        titulo: "Itens da cesta",
+        lista: [
+            {
+                nome: "Tomate",
+                imagem: tomate,
+            },
+            {
+                nome: "Brócolis",
+                imagem: brocolis,
+            },
+            {
+                nome: "Batata",
+                imagem: batata,
+            },
+            {
+                nome: "Pepino",
+                imagem: pepino,
+            },
+            {
+                nome: "Abóbora",
+                imagem: abobora,
+            },
+        ]
+    },
 }
 
 export default cesta;
--- src/telas/Cesta/index.js
+++ src/telas/Cesta/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 
 import Detalhes from './componentes/Detalhes';
 import Topo from './componentes/Topo';
+import Itens from './componentes/Itens';
 
-export default function Cesta({ topo, detalhes }) {
-    return <>
+export default function Cesta({ topo, detalhes, itens }) {
+    return <ScrollView>
     
     <Topo {...topo}/>
 
     <View style={estilos.cesta}>
-        <Detalhes {...detalhes}/> 
+        <Detalhes {...detalhes}/>
+        <Itens {...itens}/> 
     </View>
-    </>
+    </ScrollView>
 }
 
 const estilos = StyleSheet.create({
